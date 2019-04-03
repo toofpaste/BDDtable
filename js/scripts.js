7ecd97f82283abaac3640f63cfc4d9006a815d87
Michael Larragueta and Marina Poltorak <michael@>
2019-04-03 09:46:39 -0700

    Add everything

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var input = ["b", "i", "o"];
+    finalInput = [];
+
+    for(var x = 0; x < 3; x++){
+      for (var s = 1; s < 6; s++){
+      var tempInput = $("input#" + input[x] + s).val();
+      finalInput.push(tempInput);
+      };
+    };
+      console.log(finalInput);
+      var count = 0;
+
+
+
+    for(var i = 0; i < 3; i++){
+      for (var c = 0; c < 5; c++){
+      $("." + input[i] + (c+1)).append(finalInput[count]);
+      count++;
+      };
+    };
+
+    $("#output").show();
+    event.preventDefault();
+  });
+});
